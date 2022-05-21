224f418fad31ef01c28078abc5685dd09a340d4a
Aboubakry BA <boubakry01@gmail.com>
2022-05-21 22:19:52 +0000

    gestion des mots de passes de 5 caractères

--- Crackage/BruteForce.java
+++ Crackage/BruteForce.java
@@ -2,9 +2,9 @@ public class BruteForce implements Attack {
 
     // @Override
     public void attack(String psw) {
-        char tab[] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        char tab[] = { /*'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
                 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',*/ '0', '1', '2', '3', '4', '5', '6', '7', '8',
                 '9' };
         for (int i = 0; i < tab.length; i++) {
             String test = String.valueOf(tab[i]);
@@ -14,10 +14,18 @@ public class BruteForce implements Attack {
                     String test3 = test2 + "" + String.valueOf(tab[k]);
                     for (int l = 0; l < tab.length; l++) {
                         String test4 = test3 + "" + String.valueOf(tab[l]);
-                        System.out.println(test4 + "\n");
+                        System.out.println(test4);
                         if (test4.equals(psw)) {
                             System.out.println("Le mot de passe est " + test4);
                             return;
+                        }
+                        for (int m = 0; m < tab.length; m++) {
+                            String test5 = test4 + "" + String.valueOf(tab[m]);
+                            System.out.println(test5);
+                            if (test5.equals(psw)) {
+                                System.out.println("Le mot de passe est " + test5);
+                                return;
+                            }
                         }
                     }
                 }
